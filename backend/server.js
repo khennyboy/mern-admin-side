21fc753a1215c37a682a9022806e4152990d023f
khennyboy <abdullateefkehinde848@gmail.com>
2026-09-24 02:37:21 +0100

    fix file

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "..", "frontend/dist")));
 
   app.get("/*splat", (_, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
   });
 }
 
